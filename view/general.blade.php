341efaf02bb568f995f51ebbe3f15fc74a00257e
Olexandr Ryabchuk <caxa09@outlook.com>
2018-08-05 18:31:25 +0300

    create general blade

---
+++ view/general.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Camagru - General page</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+</head>
+<body>
+<?php include 'header.blade.php';  ?>
+
+</body>
+</html>
